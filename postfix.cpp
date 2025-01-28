813340b1afc2c11bf25347e798c30f527ede9e87
paigeogden2501 <160204790+paigeogden2501@users.noreply.github.com>
2025-01-28 09:49:37 -0500

    Create postfix.cpp

---
+++ postfix.cpp
@@ -0,0 +1,42 @@
+// paige ogden
+// 
+// 4/3/2024
+// 
+// file: postfix.cpp
+// read infix from file and output the postfix to postfixOutput.txt
+
+#include "utilities.hpp"
+
+int main(int argc, char const* argv[]) {
+
+    // the input file is not specified
+    if (argc < 2) {
+        std::cerr << "User did not enter an input file\n";
+    }
+
+
+    // opening input file
+    std::ifstream ifile (argv[1]);
+    // if the file was not open correctly
+    if (!ifile) {
+        std::cerr << "Unable to open the input file\n";
+    }
+
+    // opening output file
+    std::ofstream ofile(argv[2]);
+
+    // if an output file is not specified, cout is to be used
+    if (!ofile) {
+        infixToPostfix(ifile, std::cout);
+    }
+
+    // calling the method
+    infixToPostfix(ifile, ofile);
+
+    ifile.close();
+    ofile.close();
+
+    std::cout << "\nDone testing infix to postfix\n";
+
+    return 0;
+}
